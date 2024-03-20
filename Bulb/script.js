c2248a1f55f48a4aeb13871702492ecb04aecea3
smart-hit <rohithp414@gmail.com>
2024-03-20 09:48:39 +0530

    commit

---
+++ Bulb/script.js
@@ -0,0 +1,9 @@
+function turnOnOff() {
+    let image = document.getElementById('Image');
+    if (image.src.match("ONbulb"))
+        image.src =
+"https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg";
+    else
+        image.src =
+"https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg";
+}
